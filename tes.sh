fc17ca957433f0954010d0e940da0dcef0534340
Carlos Pinochet <crpinochet@gmail.com>
2021-03-29 12:42:05 -0300

    borra llave despues de autenticar

--- tes.sh
+++ tes.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 gcloud auth activate-service-account --key-file llave.json
+rm -f llave.json && rm -f credentials.tar.gz
 gcloud config set project hopeful-vim-285617
 gcloud -q --verbosity=info app deploy --no-promote
