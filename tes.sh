0ff8320be8c9d3bf7edd8cf8c7131abf550a2e1c
Carlos Pinochet <crpinochet@gmail.com>
2021-03-29 13:37:52 -0300

    removing api-key and enc cred file after auth

--- tes.sh
+++ tes.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 gcloud auth activate-service-account --key-file llave.json
-rm -f llave.json && rm -f credentials.tar.gz
+rm -f -v llave.json
+rm -f -v api_key.py
+rm -f -v credentials.tar.gz
+rm -f -v credentials.tar.gz.enc
 gcloud config set project hopeful-vim-285617
 gcloud -q --verbosity=info app deploy --no-promote
